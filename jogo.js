0701bf0db3f063e4f41f86d7fefaf35f5f810b82
Ricardo Simas <rsimas@unigranrio.br>
2019-09-27 02:42:50 -0300

    Atribuicao dos ids no tabuleiro. Incio da logica da jogada.

--- jogo.js
+++ jogo.js
@@ -1,11 +1,13 @@
 
-
 $(document).ready( function(){
 
+    //Ocultando area do tabuleiro e deixando somente a do usuário;
     $('#user-area').show();
     $('#board-area').hide();
 
     $('#btn-start').click( function(){
+        
+        //Validação dos campos nome;
         if($('#in-name-p1').val() == ''){
             alert('Digite um nome para o player 1.');
             return false;
@@ -15,11 +17,24 @@ $(document).ready( function(){
             return false;
         }
 
+        //Atribuindo conteúdo capturado em Input para o elemento Span;
         $('#out-name-p1').html($('#in-name-p1').val());
         $('#out-name-p2').html($('#in-name-p2').val());
 
+        //Ocultando area do usuário e deixando somente tabuleiro;
         $('#user-area').hide();
         $('#board-area').show();
 
+
+
     });
+
+    $('.play').click( function(){
+        var campo_clicado = this.id;
+        play(campo_clicado);
+    });
+
+    function play(){
+        
+    }
 });
